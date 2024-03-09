b4c742288fe64647013a5610fabd4bd80fc2bf4a
Carlos Cituc <carlos.cituc@gmail.com>
2024-03-08 23:55:48 -0500

    Tests ready for refactoring

---
+++ GildedRose/GildedRose.Console/ItemLogic.cs
@@ -0,0 +1,97 @@
+﻿public class ItemLogic
+{
+    private readonly IList<Item> _items;
+
+    public ItemLogic(List<Item> items)
+    {
+        _items = items;
+    }
+
+    public IList<Item> GetItems()
+    {
+        return _items;
+    }
+
+    public void UpdateQuality()
+    {
+        for (var i = 0; i < _items.Count; i++)
+        {
+            if (_items[i].Name != "Aged Brie" && _items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (_items[i].Quality > 0)
+                {
+                    if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        _items[i].Quality = _items[i].Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (_items[i].Quality < 50)
+                {
+                    _items[i].Quality = _items[i].Quality + 1;
+
+                    if (_items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (_items[i].SellIn < 11)
+                        {
+                            if (_items[i].Quality < 50)
+                            {
+                                _items[i].Quality = _items[i].Quality + 1;
+                            }
+                        }
+
+                        if (_items[i].SellIn < 6)
+                        {
+                            if (_items[i].Quality < 50)
+                            {
+                                _items[i].Quality = _items[i].Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+            {
+                _items[i].SellIn = _items[i].SellIn - 1;
+            }
+
+            if (_items[i].SellIn < 0)
+            {
+                if (_items[i].Name != "Aged Brie")
+                {
+                    if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (_items[i].Quality > 0)
+                        {
+                            if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                _items[i].Quality = _items[i].Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        _items[i].Quality = _items[i].Quality - _items[i].Quality;
+                    }
+                }
+                else
+                {
+                    if (_items[i].Quality < 50)
+                    {
+                        _items[i].Quality = _items[i].Quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
+
+public class Item
+{
+    public string Name { get; set; }
+    public int SellIn { get; set; }
+    public int Quality { get; set; }
+}
